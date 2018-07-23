737280cc9591dd989b613ebed255331c11c887b8
Greg Garville Jr <gjgarvil@gmail.com>
2018-07-23 12:08:50 -0400

    still working on code to iterate over json object

--- src/News.jsx
+++ src/News.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-
+import axios from 'axios'
 
 const ft_api = '59cbaf20e3e06d3565778e7b8fcedd4bc8d44c6dab07fe22cf287fa6'
 const news_api = 'c0624797a07a4e1988dbc5624f00c7a0'
@@ -9,20 +9,25 @@ const postman_url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=c062
 class News extends Component{
     constructor(){
         super()
-        this.state={data: []}
+        this.state={data : []
+
+        }
 
         }
     
 
 
 componentDidMount(){
-    fetch(postman_url)
+    axios.get(postman_url)
         .then(res => {
             var data2 = {}
-            data2=JSON.stringify(res.data)
+            data2 = JSON.stringify(res.data)
+            var source = [{"source" :"id"}]
             console.log(data2)
+            // console.log(source)
+            
         })
-    }
+    } 
 
     render(){
         return(
